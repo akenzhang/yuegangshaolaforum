84c2d2c57e5002f737f6bf3827eaea3502ef5dfa
akenzhang02 <1780162380@qq.com>
2016-07-28 10:17:00 +0800

    changes 2016.7.13

--- app/src/main/java/com/mantianhong/contact/fragment/ContactHomeFragment.java
+++ app/src/main/java/com/mantianhong/contact/fragment/ContactHomeFragment.java
@@ -1,6 +1,11 @@
 package com.mantianhong.contact.fragment;
 
 import android.support.v4.app.Fragment;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.mantianhong.R;
 import com.mantianhong.common.BaseFragment;
@@ -11,6 +16,9 @@ import java.lang.reflect.Field;
  * Created by new pc on 2016/7/4.
  */
 public class ContactHomeFragment extends BaseFragment {
+
+    private WebView contact_fragment_webview;
+
     @Override
     protected int getLayout() {
         return R.layout.contact_fragment_home;
@@ -19,17 +27,29 @@ public class ContactHomeFragment extends BaseFragment {
     @Override
     protected void initView() {
 
+        contact_fragment_webview = (WebView) this.getActivity().findViewById(R.id.id_contact_fragment_webview);
+
     }
 
     @Override
     protected void initVariable() {
 
+        //初始化Webview
+        WebSettings webSettings = contact_fragment_webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        contact_fragment_webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+
+        contact_fragment_webview.loadUrl("http://www.xxsweet.com/pigshow/weixin/MyShop.html");
+
     }
 
     @Override
-    protected void initListener() {
-
-    }
+    protected void initListener() {}
 
     @Override
     protected void bindData() {
--- app/src/main/java/com/mantianhong/contact/ui/aaa.java
+++
@@ -1,7 +0,0 @@
-package com.mantianhong.contact.ui;
-
-/**
- * Created by new pc on 2016/7/4.
- */
-public class aaa {
-}
