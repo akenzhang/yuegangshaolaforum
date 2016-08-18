34f4354cd798a9b5679b51c3ba5bcfe51e369f14
akenzhang02 <1780162380@qq.com>
2016-08-19 07:27:06 +0800

    changes 2016.7.13

--- app/src/main/java/com/mantianhong/home/fragment/YuegangshaolaBaseFragment.java
+++ app/src/main/java/com/mantianhong/home/fragment/YuegangshaolaBaseFragment.java
@@ -78,7 +78,6 @@ public abstract class YuegangshaolaBaseFragment extends LazyLoadBaseFragment {
                 //mydy>4表示需要拖动到一定的高度才提示
                 if(mydy>=4){
                     if(newState==RecyclerView.SCROLL_STATE_IDLE) {
-                        //LogUtil.e("findLastVisibleItemPosition() + a1:"+(mLayoutManager.findLastVisibleItemPosition() + a1)+"  getTotalrowcont():"+articleset.getTotalrowcont());
                         if (!hasmore && newState == RecyclerView.SCROLL_STATE_IDLE && mLayoutManager.findLastVisibleItemPosition() + 1 >= Integer.valueOf(articleset.getTotalrowcont())) {
                             //弹出提示对话框
                             DialogUtil.showDialog(YuegangshaolaBaseFragment.this.getFragmentContext(),R.layout.home_fragment_loadall_dialogackground,2000);
@@ -109,8 +108,6 @@ public abstract class YuegangshaolaBaseFragment extends LazyLoadBaseFragment {
                                     adapter.notifyDataSetChanged();
                                 }
                             }.getAsync(strRequestUrl);
-
-
                         }
                     }
                 }
